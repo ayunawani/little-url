bfed83da72eb37923a13540cbe975c4970b8ee7f
ayunawani <ayu.nawani@gmail.com>
2017-05-20 22:13:08 +0530

    Implemented tiny-url with db

--- routes/url.js
+++ routes/url.js
@@ -2,30 +2,57 @@ var express = require('express')
 var router = express.Router()
 var Url = require('../models/Url')
 
-global.testDb = {}
-
+function isURL (str) {
+  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/
+  if (!regex.test(str)) {
+    return false
+  } else {
+    return true
+  }
+}
 
 /* GET users listing. */
 router.get('/*', function (req, res, next) {
-  let url = req.params[0]
-  let host = req.headers['host']
-  let id = Math.floor(Math.random() * 99999)
-  let result = {
-    original_url: url,
-    short_url: `https://${host}/${id}`
+  if (global.db) {
+    let url = req.params[0]
+
+    if (!isURL(url)) {
+      res.send({
+        'error': 'Wrong url format, make sure you have a valid protocol and real site.'
+      })
+      return
+    }
+
+    let host = req.headers['host']
+    let id = Math.floor(Math.random() * 99999)
+
+    let result = {
+      original_url: url,
+      short_url: `https://${host}/${id}`
+    }
+
+    let website = {
+      id: id,
+      url: url
+    }
+
+    let collection = db.collection('websites')
+
+    collection.find({'url': url}).toArray((err, documents) => {
+      if (err) throw err
+      if (documents.length == 0) {
+        collection.insert(website, (err, data) => {
+          if (err) throw err
+          res.send(result)
+        })
+      } else {
+        res.send({
+          original_url: url,
+          short_url: `https://${host}/${documents[0].id}`
+        })
+      }
+    })
   }
-
-  testDb[id] = url 
-
-  // let urlObj = new Url({
-  //   id: id,
-  //   url: url
-  // })
-
-  // urlObj.save((err) => {
-  //   if (err) return console.log(err)
-    res.send(result)
-  // })
 })
 
 module.exports = router
