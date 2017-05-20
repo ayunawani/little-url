0d7042938014b983e44c2295a6457e6cae27f224
ayunawani <ayu.nawani@gmail.com>
2017-05-20 22:22:49 +0530

    index.html added

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  res.send("Welcome to little-url")
+  res.sendFile(process.cwd() + '/public/html/index.html')
 });
 
 module.exports = router;
